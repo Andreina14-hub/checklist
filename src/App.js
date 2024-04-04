a0f930a556fd9c8035d6d699ae26ab44bd94ecc8
Andreina14-hub <84418323+Andreina14-hub@users.noreply.github.com>
2024-04-05 01:27:52 +0200

    proyecto lista de tares

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './component/Header';
+import FormTask from './component/FormTask';
+import ListTask from './component/ListTask';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // Obtenemos las tasks guardadas de localstorage.
+    const taskSave =
+        localStorage.getItem('tasks') ?
+            JSON.parse(localStorage.getItem('tasks')) : [];
+    // Establecemos el estado de las tasks.
+    const [tasks, seTasks] = useState(taskSave);
+    // Guardando el estado dentro de localstorage
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+    // Accedemos a localstorage y comprobamos si ShowComplet es null
+    let configShowComplet = '';
+    if (localStorage.getItem('showComplet') === null) {
+        configShowComplet = true;
+    } else {
+        configShowComplet = localStorage.getItem('showComplet') === 'true';
+    }
+
+    // El estado de ShowComplet
+    const [showComplet, setShowComplet] = useState(configShowComplet);
+
+    useEffect(() => {
+        localStorage.setItem('showComplet', showComplet.toString());
+    }, [showComplet]);
+
+    return (
+        <div className="contenedor">
+            <Header
+                showComplet={showComplet}
+                setShowComplet={setShowComplet}
+            />
+            <FormTask tasks={tasks} seTasks={seTasks} />
+            <ListTask
+                tasks={tasks}
+                seTasks={seTasks}
+                showComplet={showComplet}
+            />
+        </div>
+    );
 }
-
-export default App;
+export default App; 
